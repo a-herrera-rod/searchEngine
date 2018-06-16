c38cecb1bf53eda7ab32fa5ff55693236d4e78b0
a-herrera-rod <a.herrera.rod@gmail.com>
2018-06-15 21:05:28 -0500

    Initial commit

---
+++ searchfight/Engine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace searchfight
+{
+    public class Engine
+    {
+        public string id;
+        public string url;
+        public string key;
+        public string specificEngineId;
+    }
+}
---
+++ searchfight/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace searchfight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {   
+                Console.WriteLine(search(args[0]));
+            }
+            else
+            {
+                Console.WriteLine("Please write your search term: ");
+                string searchTerm = Console.ReadLine();
+                Console.WriteLine(search(searchTerm));
+            }                
+                
+            Console.ReadLine();
+        }
+
+        public static string search(string value)
+        {
+            Engine google = new Engine()
+            {
+                id = "Google",
+                url = "https://www.googleapis.com/customsearch/v1?q={0}&cx={1}&key={2}",
+                key = "AIzaSyB5jH9MO3og3hNBTLLWLtgLH8AY6mK1PXw",
+                specificEngineId = "008769807058408683812:zm9ptt-wlcs"
+            };
+
+            string result = "";
+            string requestUrl = String.Format(google.url, value, google.specificEngineId, google.key);
+            HttpProvider http = new HttpProvider(requestUrl);
+            result = http.GetResponse();
+
+            return result;
+        }
+    }
+}
